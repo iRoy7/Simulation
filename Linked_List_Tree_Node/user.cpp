aaea7b3814c0cc80a5922db67e2f7d72c5602bc6
iRoy7 <myoungwoon@gmail.com>
2017-07-22 08:15:35 +0900

    Update user.cpp
    Update coding convention and comments

--- Linked_List_Tree_Node/user.cpp
+++ Linked_List_Tree_Node/user.cpp
@@ -4,22 +4,22 @@
 const int MAX_NODE = 10000;
 const int MAX_HASH = 100;
 NODE Nodes[MAX_NODE];
-NODE *Hash[MAX_HASH];
+NODE* Hash[MAX_HASH];
 inline int HASH(int id) { return id % MAX_HASH; }
 
 int NodeCnt = 0;
 
 int GetCurNodeCnt() { return NodeCnt; }
-int GetValueSum(int id) { NODE *n = GetNode(id);	return n->sum;}
+int GetValueSum(int id) { NODE* n = GetNode(id);	return n->sum;}
 
-NODE *GetNode(int id)
+NODE* GetNode(int id)
 {
 	if (NodeCnt == 0) for (int i = 0; i < MAX_HASH; i++) Hash[i] = 0;
 
 	// find 'id' node via HASH
 	int h = HASH(id);
 	//printf("[][MW] Hash value = (%d)\n", h);
-	NODE *n = Hash[h];
+	NODE* n = Hash[h];
 	// IF there is node having same hash value, 
 	// find exact Node and return
 	while (n)
@@ -33,13 +33,13 @@ NODE *GetNode(int id)
 	n = &Nodes[NodeCnt++];
 	n->id = id; n->child = 0;
 	// Hashing
-	n->next = Hash[h];
-	Hash[h] = n;
+	n->next = Hash[h]; // rearrange current pointer value to 1038 node to 138 new node's next
+	Hash[h] = n; // point current pointer of Hash[h] to 138 new node.
 
 	return n;
 }
 
-void ReflectParent(NODE *node, int value)
+void ReflectParent(NODE* node, int value)
 {
 	while (node)
 	{
@@ -52,15 +52,16 @@ int AddNode(int id, int pid, int value)
 {
 	if (pid == -1) NodeCnt = 0;
 
-	NODE *n = GetNode(id);
-	NODE *pn = (pid != -1) ? GetNode(pid) : 0;
+	NODE* n = GetNode(id);
+	NODE* pn = (pid != -1) ? GetNode(pid) : 0;
 	n->id = id; n->value = value; n->sum = value;
 	n->parent = pn;
 	n->sibling = (pn) ? pn->child : 0;
 
-	if (pn)
+  if (pn)
 	{
 		pn->child = n;
+    // Update upper tree value after adding new node.
 		ReflectParent(pn, value);
 		return pn->sum;
 	}
@@ -68,18 +69,20 @@ int AddNode(int id, int pid, int value)
 	return value;
 }
 
-void RemoveTree(NODE *n)
+void RemoveTree(NODE* n)
 {
-	NODE *ch = n->child;
+	NODE* ch = n->child;
+  // Recursive call for sibling node tree
 	while (ch) 
 	{ 
-		NODE *current = ch; 
+		NODE* current = ch; 
 		ch = ch->sibling;
 		RemoveTree(current);
 	}
 
+  // Update hash table
 	int hashVal = HASH(n->id);
-	NODE *h = Hash[hashVal];
+	NODE* h = Hash[hashVal];
 	if (h == n) Hash[hashVal] = h->next;
 	else
 	{
@@ -91,7 +94,7 @@ void RemoveTree(NODE *n)
 
 int RemoveNode(int id)
 {
-	NODE *n = GetNode(id);
+	NODE* n = GetNode(id);
 
 	if (n->parent)
 	{
@@ -101,7 +104,7 @@ int RemoveNode(int id)
 			n->parent->child = n->sibling;
 		else
 		{
-			NODE *p = n->parent->child;
+			NODE* p = n->parent->child;
 			while (p->sibling != n) p = p->sibling;
 			p->sibling = n->sibling;
 		}
@@ -121,13 +124,14 @@ int RemoveNode(int id)
 
 int MoveNode(int id, int pid)
 {
-	NODE *n = GetNode(id);
+	NODE* n = GetNode(id);
+  
 	// (1) Remove
 	if (n->parent->child == n)
 		n->parent->child = n->sibling;
 	else
 	{
-		NODE *p = n->parent->child;
+		NODE* p = n->parent->child;
 		while (p->sibling != n) p = p->sibling;
 		p->sibling = n->sibling;
 	}
@@ -135,7 +139,7 @@ int MoveNode(int id, int pid)
 	ReflectParent(n->parent, -(n->sum));
 
 	// (2) Add
-	NODE *pn = GetNode(pid);
+	NODE* pn = GetNode(pid);
 	n->parent = pn;
 	n->sibling = pn->child;
 	pn->child = n;
