9be3853eb9fb57492a4335a1585c92ab573e91fb
iRoy7 <myoungwoon@gmail.com>
2018-03-24 10:02:26 +0900

    Update main.cpp

--- Linked_List_Tree_Node/main.cpp
+++ Linked_List_Tree_Node/main.cpp
@@ -1,5 +1,14 @@
+// Linked_List_Tree_Node.cpp: 콘솔 응용 프로그램의 진입점을 정의합니다.
+//
+
+#include "stdafx.h"
 #include <stdio.h>
-#include "Common.h"
+
+extern int AddNode(int id, int pid, int value);
+extern int GetCurNodeCnt();
+extern int RemoveNode(int id);
+extern int GetValueSum(int pid);
+extern int MoveNode(int id, int pid);
 
 int main()
 {
@@ -42,7 +51,8 @@ int main()
 		case 4: // Sum Value
 		{
 			int id; scanf("%d", &id);
-			printf("Sum value of all child including itself = (%d)\n",GetValueSum(id));
+			printf("Sum value of all child including itself = (%d)\n", GetValueSum(id));
+			break;
 		}
 		default:
 			printf("Wrong command, move to next command\n");
